a6580b42f00c39d0024773916c8a4616b5d0f96a
Unknown <ryousuke.s.m@gmail.com>
2018-06-21 18:04:38 +0900

    add: params

--- b_cnn/config/bcnn_params.py
+++ b_cnn/config/bcnn_params.py
@@ -7,3 +7,12 @@ class BCNN_Params:
     def __init__(self, **params):
         self.log_path = Path(params.pop('log_path', MyPath.log_filepath))
         self.model_path = Path(params.pop('model_path', MyPath.model_path))
+
+        #--- coarse 1 classes ---
+        self.num_c_1      = 2  # coarse 1 classes
+        self.num_c_2      = 7  # coarse 2 classes
+        self.num_classes  = 10  # fine classes
+
+        self.batch_size   = 128
+        # self.epochs       = 60
+        self.epochs       = 1
